7da504bbdede1cf5d66dbad73d662270124737d7
HJfod <60038575+HJfod@users.noreply.github.com>
2020-05-14 18:22:26 +0300

    multiple new icons, code changes, stuff

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { BrowserWindow, app } = require("electron");
+const { BrowserWindow, app, shell } = require("electron");
 const ipc = require("electron").ipcMain;
 const path = require("path");
 const fs = require("fs");
@@ -6,7 +6,7 @@ const zlib = require("zlib")
 
 // require(path.join(__dirname,"scripts/save.js"));
 
-const dim = { w: 350, h: 450 };
+const dim = { w: 400, h: 550 };
 
 let w_main;
 let gd_path;
@@ -47,11 +47,11 @@ if (readset.length > 1) {
 }
 
 app.on("ready", () => {
-	w_main = new BrowserWindow({frame: true, height: dim.h, width: dim.w, webPreferences: { preload: path.join(__dirname, "scripts/preload.js"), nodeIntegration: false, enableRemoteModule: false, contextIsolation: true } });
+	w_main = new BrowserWindow({frame: true, icon: path.join(__dirname,"resources/icon.ico"), height: dim.h, width: dim.w, webPreferences: { preload: path.join(__dirname, "scripts/preload.js"), nodeIntegration: false, enableRemoteModule: false, contextIsolation: true } });
 	
 	w_main.loadFile("main.html");
 	
-	//w_main.setMenu(null);
+	w_main.setMenu(null);
 	
 	w_main.on("closed", () => {
 		app.quit();
@@ -60,6 +60,7 @@ app.on("ready", () => {
 
 ipc.on("app", (event, arg) => {
 	arg = JSON.parse(arg);
+	console.log(arg.action);
 	switch (arg.action) {
 		case "return":
 			w_main.webContents.send("app", `{ "action": "return", "text": "This is some text." }`);
@@ -78,11 +79,19 @@ ipc.on("app", (event, arg) => {
 				validateGDPath();
 			}
 			break;
+		case "open-link":
+			console.log(arg.link);
+			shell.openExternal(arg.link);
+			break;
 		case "get-level":
 			getLevel(arg.name);
 			break;
 		case "import-level":
 			importLevel(arg.path);
+			break;
+		case "open-folder":
+			require('child_process').exec('start ' + path.join(__dirname + dLoop,arg.folder));
+			break;
 	}
 });
 
@@ -94,17 +103,30 @@ function xor(str, key) {
 }
 
 function validateGDPath() {
-	saveData = fs.readFileSync(gd_path, "utf8");
+	w_main.webContents.send("app", `{ "action": "loading", "a": "show", "text": "Loading your GD data..." }`);
+	try {
+		saveData = fs.readFileSync(gd_path, "utf8");
+	} catch (err) {
+		console.log(err);
+		w_main.webContents.send("app", `{ "action": "loading", "a": "error", "lgt": "long", "text": "${err}" }`);
+		return;
+	}
 	
 	if (!saveData.startsWith('<?xml version="1.0"?>')) {
 		console.log("Decoding save data...")
 		saveData = xor(saveData, 11)
 		saveData = Buffer.from(saveData, 'base64')
 		try { saveData = zlib.unzipSync(saveData).toString() }
-		catch (e) { return console.log("Error! GD save file seems to be corrupt!") }
+		catch (e) {
+			w_main.webContents.send("app", `{ "action": "loading", "a": "error", "lgt": "long", "text": "${e}" }`);
+			console.log("Error! GD save file seems to be corrupt!") 
+			return;
+		}
 	}
 	
 	console.log("Save data decoded!");
+	
+	w_main.webContents.send("app", `{ "action": "loading", "a": "success", "lgt": "normal", "text": "Save data loaded!" }`);
 	
 	let c = fs.readFileSync(path.join(__dirname,dLoop,"/userdata." + ext), "utf8");
 	let o = {};
@@ -115,32 +137,68 @@ function validateGDPath() {
 	fs.writeFileSync(path.join(__dirname,dLoop,"/userdata." + ext), JSON.stringify(o));
 	
 	w_main.webContents.send("app", `{ "action": "gd-path-confirmed" }`);
-
+	
+	levels = [];
 	let levelList = saveData.match(/<k>k_\d+<\/k>.+?<\/d>\n? *<\/d>/gs)
 	levelList.forEach(lvl => {
 		levels.push(lvl)
-	})
+	});
+	
+	let levelNameList = [];
+	levels.forEach((lvl) => {
+		n = lvl.split(`<k>k2</k><s>`).pop();
+		levelNameList.push(n.substring(0,n.indexOf("<")).replace(/'/g,'"'));
+	});
+	w_main.webContents.send("app", `{ "action": "level-list", "list": "${levelNameList}" }`);
 }
 
-function getLevel(name) {
-	name = name.toLowerCase()
-	let foundLevel = levels.find(x => x.toLowerCase().includes(`<k>k2</k><s>${name}</s>`));
-	if (!foundLevel) return console.log("Could not find level!")
-	else {
-		fs.writeFileSync(path.join(__dirname,dLoop,`levels/${name}.gmd`), foundLevel.replace(/<k>k_\d+<\/k>/, ""), 'utf8');
-		return console.log(`Saved to levels/${name}.gmd!`);
+function getLevel(names) {
+	names = names.split(",");
+	if (typeof names  !== 'object'){
+		names = [names];
 	}
+	names.forEach(name => {
+		name = name.toLowerCase()
+		let foundLevel = levels.find(x => x.toLowerCase().includes(`<k>k2</k><s>${name}</s>`));
+		if (!foundLevel){
+			w_main.webContents.send("app", `{ "action": "loading", "a": "error", "lgt": "long", "text": "Level does not exist." }`);
+			console.log("Could not find level!");
+			return;
+		}
+		else {
+			fs.writeFileSync(path.join(__dirname,dLoop,`levels/${name}.gmd`), foundLevel.replace(/<k>k_\d+<\/k>/, ""), 'utf8');
+			
+			let msg = "";
+			if (names.length > 1){
+				msg = "all to levels"
+			}else{
+				msg = "to levels/" + name + ".gmd";
+			}
+			w_main.webContents.send("app", `{ "action": "loading", "a": "success", "lgt": "long", "text": "Succesfully exported ${msg}!" }`);
+			console.log(`Saved to levels/${name}.gmd!`);
+			return;
+		}
+	});
 }
 
 function importLevel(path) {
-	if (!fs.existsSync(path)) return console.log("Could not open this file!")
+	try { fs.accessSync(path) } catch(err) {
+		w_main.webContents.send("app", `{ "action": "loading", "a": "error", "lgt": "long", "text": "${err}" }`);
+		console.log("Could not open this file!");
+		return;
+	}
 	let levelFile = fs.readFileSync(path, 'utf8')
 	let levelName = levelFile.match(/<k>k2<\/k><s>(.+?)<\/s>/)
+	
+	saveData = saveData.replace(/<k>k1<\/k><i>\d+?<\/i>/g,"");	// remove uploaded id
 	saveData = saveData.split("<k>_isArr</k><t />")
-	saveData[1] = saveData[1].replace(/<k>k_(\d+)<\/k><d><k>kCEK<\/k>/g, function(n) { return "<k>k_" + (Number(n.slice(5).split("<")[0])+1) + "</k><d><k>kCEK</k>" })
+	saveData[1] = saveData[1].replace(/<k>k_(\d+)<\/k><d><k>kCEK<\/k>/g, (n) => { return "<k>k_" + (Number(n.slice(5).split("<")[0])+1) + "</k><d><k>kCEK</k>" })
 	saveData = saveData[0] + "<k>_isArr</k><t /><k>k_0</k>" + levelFile + saveData[1]
+	
 	fs.writeFileSync(gd_path, saveData, 'utf8')
-	return console.log(`Successfully added ${levelName[1]} to save file!`)
+	
+	console.log(`Successfully added ${levelName[1]} to save file!`);
+	w_main.webContents.send("app", `{ "action": "loading", "a": "success", "lgt": "long", "text": "Succesfully imported ${levelName[1]}!" }`);
 }
 
 /*
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,14 @@
 const pathInput = document.getElementById("path-input");
-const lvlGetInput = document.getElementById("level-input");
+const lvlGetInput = document.getElementById("level-list");
 const lvlImpInput = document.getElementById("level-import");
+const loading = document.getElementsByClassName("status-full")[0];
+
+const loading_time = 1000;
+const level_ext = ".gmd";
+
+let level_list = [];
+
+loading.style.display = "none";
 
 function ipcSend(msg) {
     window.postMessage({
@@ -9,12 +17,25 @@ function ipcSend(msg) {
     });
 }
 
+function arr(list) {
+    return Array.prototype.slice.call(list);
+}
+
+function changeGDPath() {
+	pathInput.disabled = false;
+	pathInput.select();
+	document.getElementById("set-path").style.display = "initial";
+	document.getElementById("edit-path").style.display = "none";
+	document.querySelector(`button[onclick="tab('import')"]`).setAttribute("disabled","");
+	document.querySelector(`button[onclick="tab('export')"]`).setAttribute("disabled","");
+}
+
 function getGDPath() {
 	ipcSend({ action: "gd-path", path: pathInput.value });
 }
 
 function getLevel() {
-	ipcSend({ action: "get-level", name: lvlGetInput.value });
+	ipcSend({ action: "get-level", name: [...lvlGetInput.options].filter(option => option.selected).map(option => option.value).join(",") });
 }
 
 function importLevel() {
@@ -23,25 +44,119 @@ function importLevel() {
 	}
 }
 
+function openLink(l) {
+	ipcSend({ action: "open-link", link: l });
+}
+
+function closeLevel() {
+	document.getElementById("drop-zone").style.display = "initial";
+	document.getElementById("level-zone").style.display = "none";
+	lvlImpInput.value = null;
+	//document.getElementsByClassName("input-text")[0].innerHTML = "Click or drop a .gmd file here!";
+	//document.getElementsByClassName("input-text")[0].classList.remove("inputted");
+	//document.getElementsByClassName("input-box")[0].classList.remove("inputted");
+}
+
 function fileDropped() {
-	document.getElementsByClassName("input-text")[0].innerHTML = lvlImpInput.files[0].path.replace(/\\/g,"/");
-	document.getElementsByClassName("input-text")[0].classList.add("inputted");
-	document.getElementsByClassName("input-box")[0].classList.add("inputted");
+	//document.getElementsByClassName("input-text")[0].innerHTML = lvlImpInput.files[0].path.replace(/\\/g,"/");
+	//document.getElementsByClassName("input-text")[0].classList.add("inputted");
+	//document.getElementsByClassName("input-box")[0].classList.add("inputted");
+	if (!lvlImpInput.files[0].name.endsWith(level_ext)){
+		showMsg("error", "normal", "You can only import .gmd files.");
+		return;
+	}
+	document.getElementById("level-name").innerHTML = lvlImpInput.files[0].path.replace(/\\/g,"/");
+	document.getElementById("drop-zone").style.display = "none";
+	document.getElementById("level-zone").style.display = "initial";
 }
 
 function tab(to) {
 	let t = document.querySelectorAll("div[data-id]");
+	let b = document.querySelectorAll(".nav-button");
 	t.forEach(i => {
-		console.log(t);
 		i.style.display = 'none';
 	});
-	console.log(`div[data-id="${to}"]`);
+	b.forEach(i => {
+		i.classList.remove("tab-selected");
+	});
 	document.querySelector(`div[data-id="${to}"]`).style.display = 'initial';
+	document.querySelector(`button[onclick="tab('${to}')"]`).classList.add("tab-selected");
+}
+
+function showMsg(type, lgt, txt) {
+	switch (type){
+		case "error":
+			loading.style.display = "initial";
+			loading.querySelector(".loading-text").innerHTML = txt;
+			loading.querySelector("#l-yes").style.display = "none";
+			loading.querySelector("#l-no").style.display = "initial";
+			loading.querySelector("#l-circle").style.display = "none";
+			break;
+		case "success":
+			loading.style.display = "initial";
+			loading.querySelector(".loading-text").innerHTML = txt;
+			loading.querySelector("#l-yes").style.display = "initial";
+			loading.querySelector("#l-no").style.display = "none";
+			loading.querySelector("#l-circle").style.display = "none";
+			break;
+	}
+	if (lgt === "normal"){
+		setTimeout(() => { loading.style.display = "none"; }, loading_time);
+	}else if (lgt === "long"){
+		setTimeout(() => { loading.style.display = "none"; }, loading_time * 3);
+	}
+}
+
+function section(to) {
+	let s = document.querySelector(`div[data-section="${to}"]`);
+	if (s.hasAttribute("hidden")) {
+		s.removeAttribute("hidden")
+	}else{
+		s.setAttribute("hidden","");
+	}
+}
+
+function folder(which) {
+	ipcSend({ action: "open-folder", folder: which });
+}
+
+function searchList(e) {
+	let a = true;
+	lvlGetInput.childNodes.forEach(i => {
+		if (i.innerHTML.indexOf(e.target.value) === -1){
+			i.setAttribute("hidden","");
+		}else{
+			i.removeAttribute("hidden");
+			a = false;
+		}
+	});
+	if (a) {
+		let o = document.createElement("option");
+		o.innerHTML = "No levels found.";
+		o.setAttribute("disabled","");
+		o.setAttribute("id","no-options");
+		lvlGetInput.appendChild(o);
+	}else{
+		if (document.querySelector("#no-options")){
+			lvlGetInput.removeChild(document.querySelector("#no-options"));
+		}
+	}
+}
+
+function selectLevel() {
+	let b = document.querySelector("#level-get");
+	if (lvlGetInput.value){
+		b.removeAttribute("disabled");
+		let sel = [...lvlGetInput.options].filter(option => option.selected).map(option => option.value);
+		b.innerHTML = "Export " + sel.join(", ");
+	}else{
+		b.setAttribute("disabled","");
+		b.innerHTML = "No level selected";
+	}
 }
 
 window.addEventListener("message", event => {
     const message = event.data;
-	
     if (message.protocol === "from-app") {
         let args = JSON.parse(message.data);
         switch (args.action) {
@@ -53,10 +168,38 @@ window.addEventListener("message", event => {
 				break;
 			case "gd-path-confirmed":
 				pathInput.disabled = true;
+				document.getElementById("set-path").style.display = "none";
+				document.getElementById("edit-path").style.display = "initial";
+				document.querySelector(`button[onclick="tab('import')"]`).removeAttribute("disabled");
+				document.querySelector(`button[onclick="tab('export')"]`).removeAttribute("disabled");
 				break;
+			case "loading":
+				if (args.a === "show"){
+					loading.style.display = "initial";
+					loading.querySelector(".loading-text").innerHTML = args.text;
+					loading.querySelector("#l-yes").style.display = "none";
+					loading.querySelector("#l-no").style.display = "none";
+					loading.querySelector("#l-circle").style.display = "initial";
+				}else if (args.a === "error" || "success"){
+					showMsg(args.a,args.lgt,args.text);
+				}else{
+					loading.style.display = "none";
+				}
+				break;
+			case "level-list":
+				level_list = args.list.split(",");
+				level_list.forEach(i => {
+					let o = document.createElement("option");
+					o.innerHTML = i;
+					lvlGetInput.appendChild(o);
+				});
+				document.querySelector("#level-amount").innerHTML = "Level count: " + level_list.length;
         }
     }
 });
 
+document.getElementById("edit-path").style.display = "none";
+document.getElementById("level-zone").style.display = "none";
+
 ipcSend({ action: "init" });
-tab("import");
+tab("path");
